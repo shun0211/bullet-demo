a1bc3b295270d2d341e0185acc7826af33b5b2c8
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-04-03 07:34:44 +0900

    bullet導入完了

---
+++ app/views/comments/_comment.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! comment, :id, :name, :post_id, :integer, :created_at, :updated_at
+json.url comment_url(comment, format: :json)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  post = Post.create!(name: "投稿-#{n}")
+  Comment.create!(content: "コメント-1", post: post)
+  Comment.create!(content: "コメント-2", post: post)
+  Comment.create!(content: "コメント-3", post: post)
+end
